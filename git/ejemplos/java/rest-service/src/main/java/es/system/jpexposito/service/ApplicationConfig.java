b6b590c776365e6e6b046b1fe65efecb8d2d3a6a
Joatham Pérez Expósito <jpe.gsc@gmail.com>
2022-10-21 12:23:43 +0100

    Se reestructura la carpeta de tareas de ide y se crea la de git

--- git/ejemplos/java/rest-service/src/main/java/es/system/jpexposito/service/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package es.system.jpexposito.service;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class ApplicationConfig extends Application {
-}
